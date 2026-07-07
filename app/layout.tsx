cf04e96e9130fe9a829abf7a16112ad7ed597a6c
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-07-07 16:23:36 +0530

    first commitswjss

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,20 +28,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${outfit.variable} font-sans antialiased`}>
         {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '828427233146959');
-            fbq('track', 'PageView');
-          `}
-        </Script>
         <noscript>
           <img
             height="1"
@@ -52,6 +38,21 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Meta Pixel Code */}
+        
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18073508286"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18073508286');
+          `}
+        </Script>
         
         {children}
         <Analytics />
